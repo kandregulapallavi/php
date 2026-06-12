79dcff9cecd05e33e4e8bbfd2392452011a6757b
kandregulapallavi <kandregula.ramkrishna127630@marwadiuniversity.ac.in>
2026-06-12 13:02:29 +0530

    Add files via upload

---
+++ form.php
@@ -0,0 +1,13 @@
+<html>
+    <head>
+        <title>html form</title>
+</head>
+<body>
+    <h2>user information</h2>
+    enter name:<input type:"text"><br>
+    enter email:<input type:"text"><br>
+    enter mobileno:<input type:"text"><br>
+    enter adress:<input type:"text"><br>
+    <input type="submit">
+</body>
+</html>
